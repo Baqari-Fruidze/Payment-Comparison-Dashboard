73d0749e66ff13635ef6a5cae8077c7a6b9867d9
Baqari-Fruidze <baqarifruidze@gmail.com>
2026-07-02 21:44:16 +0400

    folder structure, query client

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-import Image from "next/image";
+"use client"
+import { supabase } from "@/lib/supabase";
 
 export default function Home() {
+  const fetchCompanies = async () => {
+    const { data, error } = await supabase.from("companies").select("*");
+    if (error) console.error(error);
+    console.log(data);
+    return data;
+  };
+  const fetchContracts = async () => {
+    const { data, error } = await supabase.from("contracts").select("*");
+    if (error) console.error(error);
+    console.log(data);
+    return data;
+  };
+  const fetchBankTransactions = async () => {
+    const { data, error } = await supabase
+      .from("bank_transactions")
+      .select("*")
+      
+    if (error) console.error(error);
+    console.log(data);
+    return data;
+  };
+
   return (
-    <h1 className="text-3xl font-bold text-red-500">Home</h1>
+    <div className="text-3xl font-bold text-red-500 flex gap-4 flex-col justify-center items-center h-screen">
+      <button onClick={fetchCompanies}>fetchCompanies</button>
+      <button onClick={fetchContracts}>fetchContracts</button>
+      <button onClick={fetchBankTransactions}>fetchBankTransactions</button>
+    </div>
   );
 }
